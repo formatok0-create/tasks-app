8282c1e7e16e5cc0dfef1a44ff3d77fe7f19da7f
Forma User <forma@example.com>
2026-03-14 01:26:27 +0100

    Add Capacitor Android platform

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tasks.app',
+  appName: 'tasks-app',
+  webDir: 'dist'
+};
+
+export default config;
